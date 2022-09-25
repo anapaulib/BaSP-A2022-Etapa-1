77f573865bc9684cc5c0c92ebb3875eed2bc3497
Ana Paula Ibaceta <anapaulib@gmail.com>
2022-09-25 13:26:16 -0300

    BaSP-W05-2022: log-in.js updated

--- Semana-05/javascript/log-in.js
+++ Semana-05/javascript/log-in.js
@@ -1,4 +1,27 @@
 window.onload = function (){
     var inputs = document.querySelectorAll ('input');
-    console.log (inputs);
+
+    for (i = 0; i < inputs.length; i++){
+        inputs[i].onfocus = function(){
+            inputs[i].style.border = '2px solid black';
+        }
+    }
+
+    inputs[0].onblur = function(){
+
+    }
+
+    inputs[1].onblur = function(){
+        if(inputs[1].value.length<8){
+            inputs[1].style.border = '2px solid red';
+            var passAlert = document.createElement('p');
+            passAlert.innerHTML = 'Password has to be 8 characters long or more';
+            passAlert.style.color = 'red';
+            var passDiv = document.getElementById('password');
+            passDiv.appendChild(passAlert);
+            inputs[1].onfocus = function(){
+                passDiv.removeChild(passAlert);
+            }
+        }
+    }
 }
